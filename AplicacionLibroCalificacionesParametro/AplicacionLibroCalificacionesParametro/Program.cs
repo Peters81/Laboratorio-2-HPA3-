dd4cd52d93133db83c81dda5bf1dac62ee3175e5
Peters <peterscristell18@gmail.com>
2026-08-31 01:17:11 -0500

    Add files via upload

---
+++ AplicacionLibroCalificacionesParametro/AplicacionLibroCalificacionesParametro/Program.cs
@@ -0,0 +1,32 @@
+﻿using AplicacionLibroCalificacionesParametro;
+using System;
+namespace LibroCalificaciones_2
+{
+    public class LibroPuebaCalificaciones
+    {
+        static void Main(string[] args)
+        {
+        
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+
+            Console.Write("Por favor ingrese el nombre del curso: ");
+            string entrada = Console.ReadLine();
+
+            // Validación de entrada
+            while (string.IsNullOrWhiteSpace(entrada))
+            {
+                Console.WriteLine(" Error: Debe ingresar un nombre válido.");
+                Console.Write("Por favor ingrese el nombre del curso: ");
+                entrada = Console.ReadLine();
+            }
+
+
+            string nombreDelCurso = entrada;
+            Console.WriteLine();
+
+            MyLibro.MostrarMensaje(nombreDelCurso);
+        }
+    }
+}
+
+
