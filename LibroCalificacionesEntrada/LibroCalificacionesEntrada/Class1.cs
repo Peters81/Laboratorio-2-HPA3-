c2cef08c65d6f1bb54559281a9f0eb5c7f2f559f
Peters <peterscristell18@gmail.com>
2026-08-31 01:17:39 -0500

    Add files via upload

---
+++ LibroCalificacionesEntrada/LibroCalificacionesEntrada/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class LibroCalificaciones
+{
+    private string nombreCurso;
+
+    public LibroCalificaciones(string nombre)
+    {
+        nombreCurso = nombre;
+    }
+
+    public string NombreCurso
+    {
+        get { return nombreCurso; }
+        set
+        {  
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                nombreCurso = "Sin Asignar";
+            }
+            else if (value.Length > 30) 
+            {
+                nombreCurso = value.Substring(0, 30); 
+            }
+            else
+            {
+                nombreCurso = value;
+            }
+        }
+    }
+
+    public void MostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al libro de calificaciones para: \n{0}!", NombreCurso);
+    }
+}
+
+
+
---
+++ LibroCalificacionesEntrada/LibroCalificacionesEntrada/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LibroCalificacionesEntrada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LibroCalificaciones myLibro = new LibroCalificaciones("CS101 Programación en C#");
+            LibroCalificaciones myLibro2 = new LibroCalificaciones("CS102 Estructuras de Datos");
+            Console.WriteLine("El nombre del curso es: {0}", myLibro.NombreCurso);
+            Console.WriteLine("El nombre del curso es: {0}", myLibro2.NombreCurso);
+
+            Console.WriteLine("\nEscriba el nombre del curso:");
+            string elNombreCurso = Console.ReadLine();
+
+            // Bucle de validación de entrada por consola
+            while (string.IsNullOrWhiteSpace(elNombreCurso))
+            {
+                Console.WriteLine(" Error: El nombre ingresado no es válido. \n Escriba un nombre:");
+                elNombreCurso = Console.ReadLine();
+            }
+
+            myLibro.NombreCurso = elNombreCurso;
+            Console.WriteLine("el nombre del curso es: {0}", myLibro.NombreCurso);
+        }
+    }
+}
+
+
+
+
