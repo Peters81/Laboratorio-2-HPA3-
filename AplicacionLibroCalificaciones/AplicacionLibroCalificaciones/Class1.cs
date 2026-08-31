256321935cfbb1aa485fac3365be408963d92e9e
Peters <peterscristell18@gmail.com>
2026-08-31 01:16:46 -0500

    Add files via upload

---
+++ AplicacionLibroCalificaciones/AplicacionLibroCalificaciones/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AplicacionLibroCalificaciones
+{ 
+
+public class LibroCalificaciones
+
+{
+    public void MostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al Libro de Calificaciones");
+    }
+}
+}
+
